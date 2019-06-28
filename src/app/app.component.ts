0d4bbd7b191b2095d89df74573d7379729e3f345
dng1999 <dorothyng1999@gmail.com>
2019-06-28 12:51:16 -0400

    now you pay in shards and get errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,45 @@ import { shard, items } from '../cleanSave';
 })
 export class AppComponent  {
   title = 'Shard Nexus';
+  error = {status: false, message:''};
 
   //JSON parse to avoid shallow copying
   shard = JSON.parse(JSON.stringify(shard));
   items = JSON.parse(JSON.stringify(items));
 
   save() {
+    window.alert('Saved');
     //making sure values taken from cleanSave aren't modifies
-    window.alert(JSON.stringify(items));
-    window.alert(JSON.stringify(shard));
+    //window.alert(JSON.stringify(items));
+    //window.alert(JSON.stringify(shard));
   };
 
   reset() {
     //parse imported data again to reset and avoid shallow copying
     this.shard = JSON.parse(JSON.stringify(shard));
     this.items = JSON.parse(JSON.stringify(items));
+    this.error.status = false;
   }
 
   addShard() {
     this.shard++;
+    this.error.status = false;
   }
 
   addItem(name) {
     var items = this.items;
     var i = 0;
     for (i; i<items.length; i++){
-      if (items[i].name == name) { items[i].bought += 1; }
+      if (items[i].name == name && items[i].price <= this.shard) {
+        this.error.status = false;
+        items[i].bought += 1;
+        this.shard -= items[i].price;
+        break;
+      }
+      else {
+        this.error.status = true;
+        this.error.message = 'You do not have enough shards for ' + name + '.';
+      }
     }
   }
 }
--- src/cleanSave.ts
+++ src/cleanSave.ts
@@ -3,14 +3,17 @@ export var shard = 0;
 export var items = [
     {
       name: 'Euphonium',
-      bought: 0
+      bought: 0,
+      price: 1
     },
     {
       name: 'Lamien',
-      bought: 0
+      bought: 0,
+      price: 10
     },
     {
       name: 'Iske',
-      bought: 0
+      bought: 0,
+      price: 50
     }
   ];
